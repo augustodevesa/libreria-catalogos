39689af2042fad68299a04c0febbc859c9ebd7e3
Agustín Benassi <agusbenassi@gmail.com>
2018-04-13 15:00:44 -0300

    Agrego campos automáticos al CSV, que provienen de la lectura del data.json de cada catálogo.

--- helpers.py
+++ helpers.py
@@ -10,6 +10,7 @@ import os
 import sys
 import json
 import pandas as pd
+from pydatajson import DataJson
 
 
 def nodes_to_csv(input_path, output_path):
@@ -27,18 +28,24 @@ def nodes_to_df(input_path):
     rows = []
     for jurisdiction in nodes["jurisdictions"]:
         for catalog in jurisdiction["catalogs"]:
+            print("Leyendo catálogo '{}' de la jurisdiccion '{}' ({})".format(
+                catalog["id"], jurisdiction["id"], jurisdiction["title"]))
+            dj = DataJson(catalog["url_json"])
+
             rows.append({
                 "jurisdiction_id": jurisdiction["id"],
                 "jurisdiction_title": jurisdiction["title"],
                 "catalog_id": catalog["id"],
+                "catalog_title": dj.get("title"),
+                "catalog_homepage": dj.get("homepage"),
                 "catalog_url_json": catalog["url_json"],
-                "catalog_url_xlsx": catalog["url_xlsx"],
-                "catalog_url_datosgobar": catalog["url_datosgobar"]
+                "catalog_url_xlsx": catalog.get("url_xlsx"),
+                "catalog_url_datosgobar": catalog.get("url_datosgobar")
             })
 
     fields = ["jurisdiction_id", "jurisdiction_title",
-              "catalog_id", "catalog_url_json", "catalog_url_xlsx",
-              "catalog_url_datosgobar"]
+              "catalog_id", "catalog_title", "catalog_homepage",
+              "catalog_url_json", "catalog_url_xlsx", "catalog_url_datosgobar"]
 
     return pd.DataFrame(rows)[fields]
 
